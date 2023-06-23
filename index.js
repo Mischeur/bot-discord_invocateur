7aaaf5feabab98f9866fc5c3cdb8d82650fdb8e5
Mischeur <lu.yannick@hotmail.com>
2023-06-23 18:23:35 +0200

    Correction par rapport au token visible par tous

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits, REST, Routes, IntentsBitField } = require('discord.js');
-const { clientId, guildId, token } = require('./config.json');
+
+const { config } = require('node:process');
+require('dotenv').config()
+const token = process.env.TOKEN
+const clientId = process.env.CLIENT_ID
+const guildId = process.env.GUILD_ID
 
 const client = new Client({
     intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessages],
