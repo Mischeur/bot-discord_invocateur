60461dbee30a246a7b581f8f6e42c373dcc57eb2
Mischeur <lu.yannick@hotmail.com>
2023-06-24 13:22:59 +0200

    ~ Création de personnage
    Changement d'un formulaire avec un collector en un modals

--- index.js
+++ index.js
@@ -92,3 +92,16 @@ client.on(Events.InteractionCreate, async interaction => {
         }
     }
 });
+
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
